c1b87372a4e1d27a654274649e3849b6f40af66f
Eric Hong <Eric.Hong@microsoft.com>
2020-11-04 09:38:16 -0800

    fix: Mount /usr/local/share/ca-certificates folder into kube-controller-manager (#4001)

--- pkg/engine/templates_generated.go
+++ pkg/engine/templates_generated.go
@@ -24332,6 +24332,9 @@ spec:
         - name: etc-ssl
           mountPath: /etc/ssl
           readOnly: true
+        - name: ca-certificates
+          mountPath: /usr/local/share/ca-certificates
+          readOnly: true
 {{end}}
         - name: var-lib-kubelet
           mountPath: /var/lib/kubelet
@@ -24349,6 +24352,9 @@ spec:
     - name: etc-ssl
       hostPath:
         path: /etc/ssl
+    - name: ca-certificates
+      hostPath:
+        path: /usr/local/share/ca-certificates
 {{end}}
     - name: var-lib-kubelet
       hostPath:
